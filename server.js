13399c3fab73cfc0c1ef32c6733470c3932dcb72
bobbygour30 <bobby30gour@gmail.com>
2025-06-09 18:03:38 +0530

    some changes

--- server.js
+++ server.js
@@ -11,22 +11,23 @@ const app = express();
 connectDB();
 
 // Custom CORS Middleware
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (origin) {
-    res.setHeader('Access-Control-Allow-Origin', origin); // Reflect the request's origin
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+// app.use((req, res, next) => {
+//   const origin = req.headers.origin;
+//   if (origin) {
+//     res.setHeader('Access-Control-Allow-Origin', origin); // Reflect the request's origin
+//   }
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
+//   if (req.method === 'OPTIONS') {
+//     return res.status(200).end();
+//   }
 
-  next();
-});
-
+//   next();
+// });
+app.use(cors());
+app.options("*" , cors());
 // Middleware
 app.use(express.json({ limit: '10mb' })); // Increase payload limit for base64 files
 
