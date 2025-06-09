c254fff18c9e57ee898029161362519be9276a9c
bobbygour30 <bobby30gour@gmail.com>
2025-06-09 17:48:43 +0530

    origin changes

--- server.js
+++ server.js
@@ -10,12 +10,11 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-// Custom CORS Middleware (replacing cors package)
+// Custom CORS Middleware
 app.use((req, res, next) => {
-  const allowedOrigins = ['https://admitcard-frontend.vercel.app', 'http://localhost:5173'];
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+  if (origin) {
+    res.setHeader('Access-Control-Allow-Origin', origin); // Reflect the request's origin
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
